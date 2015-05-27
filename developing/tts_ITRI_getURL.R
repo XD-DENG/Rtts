18a71001c5029fc73f201344e6cfbb9c5a18b60a
XD-DENG <xd_deng@hotmail.com>
2015-05-27 23:36:02 +0800

    update the new progress in function tts_ITRI

--- developing/tts_ITRI_getURL.R
+++ developing/tts_ITRI_getURL.R
@@ -1,26 +1,26 @@
 library(RCurl)
 
-tts_ITRI_getID <- function(content){
+tts_ITRI_getURL <- function(voice_ID){
   
   headerFields <- 
     c(Accept = "text/xml",
       Accept = "multipart/*",
       'Content-Type' = "text/xml; charset=utf-8",
-      SOAPAction = "http://tts.itri.org.tw/TTSService/ConvertSimple")
+      SOAPAction = "http://tts.itri.org.tw/TTSService/GetConvertStatus")
   
   body_head <- '<?xml version="1.0" encoding="utf-8"?>
   <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
     <soap:Body>
-      <ConvertSimple>
+      <GetConvertStatus>
       <accountID>test-for-r</accountID>
       <password>test1for1r</password>
-      <TTStext>'
-  body_tail <- '</TTStext>
-      </ConvertSimple>
+      <convertID>'
+  body_tail <- '</convertID>
+      </GetConvertStatus>
     </soap:Body>
   </soap:Envelope>'
   
-  body <- paste(body_head, content, body_tail, sep="")
+  body <- paste(body_head, voice_ID, body_tail, sep="")
   
   # we want the actual body of the response from the web server.
   # To get this, we provide function basicTextGatherer() to collect the text.
@@ -31,9 +31,9 @@ tts_ITRI_getID <- function(content){
               verbose = FALSE,
               writefunction = reader$update
   )
-  voice_ID_raw <- reader$value()
-  voice_ID <- strsplit(strsplit(strsplit(voice_ID_raw, split="</Result>")[[1]][1], split = 'string\">')[[1]][2], split=";")[[1]][3]
-  return(voice_ID)
+  voice_URL_raw <- reader$value()
+  voice_URL <- strsplit(strsplit(voice_URL_raw, split = "</Result>")[[1]][1] ,split = "completed&amp;")[[1]][2]
+  return(voice_URL)
 }
 
 
